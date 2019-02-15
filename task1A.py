e1ddc9c92c241b2676b8d8e28a69f4352dfe9dd8
Jude666 <zh415944@dal.ca>
2019-02-15 10:32:06 -0800

    second

--- task1A.py
+++ task1A.py
@@ -40,3 +40,31 @@ print(unique_words(d))
 
 #This is Task1A-2
 
+def break_into_words():
+
+    book = open('book1.txt', 'book2.txt', 'book3.txt')
+    words_list = []
+    for line in book:
+        for item in line.split():
+            item = del_punctuation(item)
+            item = item.lower()
+            # print(item)
+            words_list.append(item)
+    return words_list
+
+
+def count_the_article():
+   
+    words_list = break_into_words()
+    dictionary = {}
+    for word in words_list:
+        if word not in dictionary:
+            dictionary[word] = 1
+        else:
+            dictionary[word] += 1
+
+    count = len(dictionary)
+    return count
+
+print(count_the_article())
+
